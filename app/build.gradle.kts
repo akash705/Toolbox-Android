ef2b99e9bf68c4ae16d7379a5cc9e4c9efa39520
Akash Mishra <akash.mishra.dev@gmail.com>
2026-09-13 13:08:17 +0530

    Bump version to 1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.vedtechnologies.toolbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.4.1"
+        versionCode = 8
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

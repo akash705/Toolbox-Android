40f23885db7c1b0fee17f848b99621611a37f43e
Akash Mishra <akash.mishra.dev@gmail.com>
2026-09-20 10:07:45 +0530

    chore: bump version to 1.6.0 (versionCode 12)
    Not sure who

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.vedtechnologies.toolbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.5.0"
+        versionCode = 12
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
